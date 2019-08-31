ce326d53876d8de9c1d75a6bc18c8fd03fce29d4
Syed Aziz <syedaziz27x@gmail.com>
2019-08-31 12:45:42 -0400

    cells near clicked cell will also toggle now

--- src/Board.js
+++ src/Board.js
@@ -75,13 +75,17 @@ class Board extends Component {
     }
 
     flipCell(y, x);
+    flipCell(y, x - 1);
+    flipCell(y, x + 1);
+    flipCell(y + 1, x);
+    flipCell(y - 1, x);
 
     // TODO: flip this cell and the cells around it
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    let hasWon = false;
+    const hasWon = board.every(row => row.every(cell => !cell));
     this.setState({
       board,
       hasWon
@@ -91,6 +95,7 @@ class Board extends Component {
   /** Render game board or winning message. */
 
   render() {
+    if (this.state.hasWon) return <h1>You won</h1>;
     const tableBoard = [];
     for (let y = 0; y < this.props.nrows; y++) {
       const row = [];
