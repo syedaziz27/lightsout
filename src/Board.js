302d645b7aca63d1409d2e5595c08cc94e837846
Syed Aziz <syedaziz27x@gmail.com>
2019-08-31 13:21:56 -0400

    refactored render in board.js, added style to winner banner

--- src/Board.js
+++ src/Board.js
@@ -2,32 +2,6 @@ import React, { Component } from "react";
 import Cell from "./Cell";
 import "./Board.css";
 
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
-
 class Board extends Component {
   static defaultProps = {
     nrows: 5,
@@ -43,32 +17,24 @@ class Board extends Component {
     };
   }
 
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
-
   createBoard() {
     let board = [];
-    for (let i = 0; i < this.props.nrows; i++) {
-      const row = [];
-      for (let j = 0; j < this.props.ncols; j++) {
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
         row.push(Math.random() < this.props.chanceLightStartsOn);
       }
       board.push(row);
-      console.log(row);
     }
     return board;
   }
 
-  /** handle changing a cell: update board & determine if winner */
-
   flipCellsAround(coord) {
-    console.log("flipping", coord);
     let { ncols, nrows } = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
 
     function flipCell(y, x) {
-      // if this coord is actually on board, flip it
-
       if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
         board[y][x] = !board[y][x];
       }
@@ -77,49 +43,53 @@ class Board extends Component {
     flipCell(y, x);
     flipCell(y, x - 1);
     flipCell(y, x + 1);
-    flipCell(y + 1, x);
     flipCell(y - 1, x);
+    flipCell(y + 1, x);
 
-    // TODO: flip this cell and the cells around it
+    let hasWon = board.every(row => row.every(cell => !cell));
 
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
-
-    const hasWon = board.every(row => row.every(cell => !cell));
-    this.setState({
-      board,
-      hasWon
-    });
+    this.setState({ board: board, hasWon: hasWon });
   }
 
-  /** Render game board or winning message. */
-
-  render() {
-    if (this.state.hasWon) return <h1>You won</h1>;
-    const tableBoard = [];
+  makeTable() {
+    let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++) {
-      const row = [];
+      let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
-        const coord = `${y}-${x}`;
+        let coord = `${y}-${x}`;
         row.push(
           <Cell
-            isLit={this.state.board[y][x]}
             key={coord}
+            isLit={this.state.board[y][x]}
             flipCellsAroundMe={() => this.flipCellsAround(coord)}
           />
         );
       }
-      tableBoard.push(<tr key={y}>{row}</tr>);
+      tblBoard.push(<tr key={y}>{row}</tr>);
     }
     return (
+      <table className="Board">
+        <tbody>{tblBoard}</tbody>
+      </table>
+    );
+  }
+  render() {
+    return (
       <div>
-        <div className="Board-title">
-          <div className="neon-orange">Lights</div>
-          <div className="neon-blue">Out</div>
-        </div>
-        <table className="Board">
-          <tbody>{tableBoard}</tbody>
-        </table>
+        {this.state.hasWon ? (
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WIN!</span>
+          </div>
+        ) : (
+          <div>
+            <div className="Board-title">
+              <div className="neon-orange">Lights</div>
+              <div className="neon-blue">Out</div>
+            </div>
+            {this.makeTable()}
+          </div>
+        )}
       </div>
     );
   }
